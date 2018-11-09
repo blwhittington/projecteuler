ffdc985a805a8a92fa11f92dd61cc53a34adedc9
blk <blakewhittington@gmail.com>
2018-11-08 23:25:55 -0800

    Update 1_summingMultiples.java

--- 1_summingMultiples.java
+++ 1_summingMultiples.java
@@ -1,5 +1,5 @@
 class javaCup{
-/**adding multiples of 3 or 5 between one and one-thousand*/
+/*adding multiples of 3 or 5 between one and one-thousand*/
 public static void main(String[] args) {
 	int tal=0;
 	for (int num=0;num<1000;num++) {
